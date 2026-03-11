f7d8df5e1be1a516b523b8dda40d7d9d6e6581ef
ajvisuni <a.joseph.adv.corp@gmail.com>
2026-03-11 02:00:25 +0100

    Initial commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,22 +1,22 @@
 public class Task3 {
     public static void main(String[] args){
-        int amountofbricksjoseph = 67;
-        int containercapacityjoseph = 8;
+        int amountofbricks = 67;
+        int containercapacity = 8;
 
-        System.out.println("There is " + amountofbricksjoseph + " total Lego bricks.");
-        System.out.println("Each container can hold exactly " + containercapacityjoseph + " bricks");
+        System.out.println("There is " + amountofbricks + " total Lego bricks.");
+        System.out.println("Each container can hold exactly " + containercapacity + " bricks");
 
-        int fullcontainersjoseph = amountofbricksjoseph / containercapacityjoseph;
-        System.out.println("The total number of bricks in a Container are : " + fullcontainersjoseph);
+        int fullcontainers = amountofbricks / containercapacity;
+        System.out.println("The total number of bricks in a Container are : " + fullcontainers);
 
-        int partialyfilledcontainersjoseph = amountofbricksjoseph % containercapacityjoseph;//that is a long variable ;)
-        System.out.println("Number of bricks in the partialy filled contaniners: " + partialyfilledcontainersjoseph);
+        int partialyfilledcontainers = amountofbricks % containercapacity;//that is a long variable ;)
+        System.out.println("Number of bricks in the partialy filled contaniners: " + partialyfilledcontainers);
 
 
-        int totalcontainersjoseph = fullcontainersjoseph;
-        if(partialyfilledcontainersjoseph > 0){
-            totalcontainersjoseph = totalcontainersjoseph + 1;
+        int totalcontainers = fullcontainers;
+        if(partialyfilledcontainers > 0){
+            totalcontainers = totalcontainers + 1;
         }
-        System.out.println("Total number of containers used overall: " + totalcontainersjoseph);
+        System.out.println("Total number of containers used overall: " + totalcontainers);
     }
 }
--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,15 @@
+//I have made some "ASSUMPTIONS" , hope that doesn't compromise a perfect grade and this is purely after learing. NO AI USED .:)
+import java.math.BigDecimal;
+
 public class Task4 {
+    public static void main(Strings[] args){
+        double netprice = 9.99;
+        double vatrate =0.23; //23% VAT
+        double grossprice = netprice * (1 + vatrate);
+        System.out.println("Gross value (1 piece): " + grossprice);
+        int quantity = 10000;
+        double totalgrossprice = grossprice * quantity;
+        System.out.println("Total griss value of 10000 pieces : " + totalgrossprice);
+        double total
+    }
 }
